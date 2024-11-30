ea64799b92a65ca090de79b1fd357a5d29ff0401
Joakim Samuelsson <joakim.teknikhogskolan@gmail.com>
2024-11-30 18:29:17 +0100

    ..

--- LabShortestRouteFinder/View/ListViewControl.xaml.cs
+++ LabShortestRouteFinder/View/ListViewControl.xaml.cs
@@ -17,22 +17,12 @@ namespace LabShortestRouteFinder.View
         {
             if (ViewModel != null)
             {
-                // Ensure the current edit is committed
-                routesGrid.CommitEdit();
                 ViewModel.SaveRoutesToFile();
             }
             else
             {
                 MessageBox.Show("ViewModel is not properly initialized.", "Error", 
                     MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        {
-            if (e.EditAction == DataGridEditAction.Commit)
-            {
-                routesGrid.CommitEdit();
             }
         }
     }
